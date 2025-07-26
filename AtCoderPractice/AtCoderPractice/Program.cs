1520aec82d753a401dbccec2d12ba1095bbd2f20
bubbleShaker <intselectk@gmail.com>
2025-07-26 12:22:27 +0900

    AtCoder#3 Atcoder Beginner SelectionsをC#で解き終える
    - Shift onlyを宣言的っぽく書いてみる

--- AtCoderPractice/AtCoderPractice/Program.cs
+++ AtCoderPractice/AtCoderPractice/Program.cs
@@ -7,8 +7,16 @@ public class Program
 {
     public static void Main(string[] args)
     {
-        var input = Console.ReadLine();
-        var ans = input.Count(c => c == '1');
+        int n = int.Parse(Console.ReadLine());
+        List<int> a = Console.ReadLine().Split().Select(item=>int.Parse(item)).ToList();
+        int ans = Enumerable.Range(0, 100)
+            .TakeWhile(_ => {
+                if (!a.All(x => x % 2 == 0)){
+                    return false;
+                }
+                a = a.Select(item => item /= 2).ToList();
+                return true;
+            }).Count();
         Console.WriteLine(ans);
     }
 }
