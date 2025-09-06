3e8df1e93421528628ddf424f54eb8bfe33da0a4
bubbleShaker <intselectk@gmail.com>
2025-09-06 20:24:48 +0900

    ref #2 C++でAtCoderの環境構築

---
+++ AtCoderCpp/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <cmath>
+
+// Include AtCoder Library
+#include "ac-library/atcoder/all"
+
+int main() {
+    // Your code here
+    std::cout << "Hello, AtCoder!" << std::endl;
+    return 0;
+}
