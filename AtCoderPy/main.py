ac7a1905128e393e4e7c0a9d1d7ff9e7f7432b67
bubbleShaker <intselectk@gmail.com>
2025-09-06 20:23:14 +0900

    ref #2 PythonでAtCoderの環境構築

---
+++ AtCoderPy/main.py
@@ -0,0 +1,29 @@
+import sys
+from collections import deque, defaultdict, Counter
+from heapq import heappop, heappush
+from bisect import bisect_left, bisect_right
+from math import gcd, ceil, floor, factorial, sqrt
+from itertools import permutations, combinations, product, accumulate
+
+# AtCoder Library (if installed)
+# from atcoder.dsu import DSU
+# from atcoder.segtree import SegTree
+# from atcoder.lazysegtree import LazySegTree
+# from atcoder.string import suffix_array, lcp_array, z_algorithm
+# from atcoder.math import convolution, floor_sum
+# from atcoder.maxflow import MFGraph
+# from atcoder.mincostflow import MCFGraph
+# from atcoder.scc import SCCGraph
+# from atcoder.twosat import TwoSAT
+
+
+# Fast I/O
+def input():
+    return sys.stdin.readline().rstrip()
+
+def main():
+    # Your code here
+    pass
+
+if __name__ == "__main__":
+    main()
